f52fae6a85e2c0d3c008280da2f780cc2506947a
FbLolly <santangelo.lorenzo.071207@gmail.com>
2025-04-20 23:38:06 +0200

    Fixed tabs in Game

---
+++ src/gameManagement/dialogue/DIALOGUE.md
@@ -0,0 +1,11 @@
+## dialogue + num starts at map number num
+
+# structure
+
+{LOCK for lock, nothing for no lock
+NARRATOR
+
+STUFF@ (@ to \n)
+STUFF@
+OTHER STUFF
+}
--- src/mainPkg/Game.java
+++ src/mainPkg/Game.java
@@ -28,22 +28,21 @@ public class Game {
     public DeathTimer deathTimer;
     public Dialogue dialogue;
     public ObjectEventHandler oeh;
-
-    public DeathScreen ds = new DeathScreen(this);
-
+    public DeathScreen ds;
     public KeyHandler keyHandler;
     private Mouse mouse;
 
     private Particles particles;
-
     public JApp app;
 
     public Game(JApp app){
         this.dialogue = new Dialogue();
         this.map = new Map(1, this);
         this.oeh = new ObjectEventHandler();
+        this.ds = new DeathScreen(this);
 
         RayPoint playerPos = this.map.getSpawn();
+
         this.player = new Player(playerPos.x, playerPos.y, Defines.tileSize, Defines.tileSize);
         this.dog = new Dog(playerPos.x, playerPos.y, Defines.tileSize, Defines.tileSize, this.player);
         this.cam = new Camera(0, 0);
@@ -55,68 +54,63 @@ public class Game {
 
         this.particles = new Particles();
         this.app = app;
-
-        //Save.loadSave(this);
     }
-
+    
     public void updateIO(){
-      this.keyHandler.handleKeys(this.app);
-      this.mouse.updateMousePos(this.app);
+        this.keyHandler.handleKeys(this.app);
+        this.mouse.updateMousePos(this.app);
     }
 
     public void update(){
-      this.updateIO();
+        this.updateIO();
+        this.dialogue.update(this.keyHandler.KeyMap);
+        this.cam.update(this.player);
+        this.player.loadInput(this.keyHandler.KeyMap);
+        this.player.update(this);
 
-      this.dialogue.update(this.keyHandler.KeyMap);
-
-      this.cam.update(this.player);
-      this.player.loadInput(this.keyHandler.KeyMap);
-      this.player.update(this);
-      try{
-        this.dog.update(this);
-      }catch(NullPointerException e){}
-
-      this.deathTimer.update(this.map, this.player);
-      this.particles.update();
-
-      this.map.update(
-        new Entity[]{this.player, this.dog}
-      );
+        try{
+            this.dog.update(this);
+        }catch(NullPointerException e){}
+        
+        this.deathTimer.update(this.map, this.player);
+        this.particles.update();
+        this.map.update(
+            new Entity[]{this.player, this.dog}
+        );
     }
 
     public void paintComponent(Graphics g){
-      //paint background
-      g.setColor(Defines.bgc);
-      g.fillRect(0, 0, Defines.width, Defines.height);
-      //-
+        //paint background
+        g.setColor(Defines.bgc);
+        g.fillRect(0, 0, Defines.width, Defines.height);
+        //-
 
-      if (Defines.fontMetrics == null){
-        Defines.fontMetrics = g.getFontMetrics();
-        Dialogue.updateShowing();
-      }
+        if (Defines.fontMetrics == null){
+            Defines.fontMetrics = g.getFontMetrics();
+            Dialogue.updateShowing();
+        }
 
-      Zoomer.update(this);
-      Zoomer.Zoom(2);
+        Zoomer.update(this);
+        Zoomer.Zoom(2);
 
-      ((Graphics2D) g).scale(Defines.zoom, Defines.zoom);
+        ((Graphics2D) g).scale(Defines.zoom, Defines.zoom);
 
-      this.cam.translate((Graphics2D) g);
+        this.cam.translate((Graphics2D) g);
+        this.map.paintComponent(g, this.imageList, cam);
+        this.player.paintComponent(g, this.imageList);
+        
+        try{
+            this.dog.paintComponent(g, imageList);
+        }catch(NullPointerException e){}
 
-      this.map.paintComponent(g, this.imageList, cam);
-      this.player.paintComponent(g, this.imageList);
-      try{
-        this.dog.paintComponent(g, imageList);
-      }catch(NullPointerException e){}
-
-      this.particles.paintComponent(g, imageList);
-
-      this.map.paintLighting(g, this);
-      this.deathTimer.paintComponent(g, this);
-      this.player.paintInventory(g, this);
-      this.dialogue.paintComponent(g, this);
+        this.particles.paintComponent(g, imageList);
+        this.map.paintLighting(g, this);
+        this.deathTimer.paintComponent(g, this);
+        this.player.paintInventory(g, this);
+        this.dialogue.paintComponent(g, this);
     }
 
     public void changeDialogue(int dialogue){
-      this.dialogue.refresh(dialogue);
+        this.dialogue.refresh(dialogue);
     }
 }
